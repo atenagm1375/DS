94ea9601a4abfe3258fd69997bcfc33487e96bef
atena <ashenatena@gmail.com>
2016-09-29 13:41:09 +0330

    Polynomial summation for both displays and multiplication for first

--- sum_mult.cpp
+++ sum_mult.cpp
@@ -33,15 +33,54 @@ int *multiply1(int *A, int *B){
     return ans;
 }
 
+int *sum2(int *A, int *B){
+    int m = A[0], n = B[0];
+    int len = A[1] > B[1] ? A[1] : B[1];
+    int *ans = new int[2 * len + 1];
+    int i = 1, j = 1, k = 1, counter = 0;
+    while(true){
+        while(A[i] > B[j] || (i < 2 * m && j >= 2 * n)){
+            counter++;
+            ans[k++] = A[i++];
+            ans[k++] = A[i++];
+        }
+        while(B[j] > A[i] || (j < 2 * n && i >= 2 * m)){
+            counter++;
+            ans[k++] = B[j++];
+            ans[k++] = B[j++];
+        }
+        while(i < 2 * m && j < 2 * n && A[i] == B[j]){
+            counter++;
+            ans[k++] = A[i++];
+            ans[k++] = A[i++] + B[++j];
+            j++;
+        }
+        if(i >= 2 * m && j >= 2 * n)
+            break;
+    }
+    int *result = new int[2 * counter + 1];
+    result[0] = counter;
+    for(int x = 1; x <= 2 * counter; x++)
+        result[x] = ans[x];
+    return result;
+}
+
 int main(){
-    int A[6] = {4, 4, -3, 2, 0, -11};
-    int B[8] = {6, 1, 0, 0, 4, -5, 10, 6};
-    int *ans = sum1(A, B);
+    int A1[6] = {4, 4, -3, 2, 0, -11};
+    int B1[8] = {6, 1, 0, 0, 4, -5, 10, 6};
+    int *ans = sum1(A1, B1);
     for(int i = 0; i < 8; i++)
         std::cout << ans[i] << " ";
     std::cout << std::endl;
-    ans = multiply1(A, B);
+    ans = multiply1(A1, B1);
     for(int i = 0; i < 12; i++)
+        std::cout << ans[i] << " ";
+    std::cout << std::endl;
+
+    int A2[11] = {5, 10, -5, 8, -1, 7, 3, 4, 2, 0, 4};
+    int B2[13] = {6, 5, -1, 4, 3, 3, 2, 2, 3, 1, 2, 0, -2};
+    ans = sum2(A2, B2);
+    for(int i = 0; i <= 2 * ans[0]; i++)
         std::cout << ans[i] << " ";
     return 0;
 }
