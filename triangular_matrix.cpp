f0e10e70ef772b7d10bbc4bf074ca345d2a08a4d
atena <ashenatena@gmail.com>
2016-10-04 18:36:01 +0330

    HW#1

---
+++ triangular_matrix.cpp
@@ -0,0 +1,72 @@
+// Ashena Gorgan Mohammadi, 610394128
+
+#include <iostream>
+
+using namespace std;
+
+class TriangularMatrix{
+    public:
+
+        TriangularMatrix(int **A, int n){
+            dim = n;
+            size = dim * (dim + 1) / 2;
+            matrix = new int[size];
+            isLowerTriangular = true;
+
+            for(int i = 0; isLowerTriangular && i < dim; i++)
+                for(int j = i + 1; isLowerTriangular && j < dim; j++)
+                    if(A[i][j] != 0)
+                        isLowerTriangular = false;
+            if(isLowerTriangular){
+                int lim = 0;
+                for(int k = 0, i = 0; i < dim; i++, lim++)
+                    for(int j = 0; j <= lim; j++)
+                        matrix[k++] = A[i][j];
+            }
+            else{
+                for(int k = 0, i = 0; i < dim; i++)
+                    for(int j = i; j < dim; j++)
+                        matrix[k++] = A[i][j];
+            }
+        }
+
+        void print(){
+            if(isLowerTriangular){
+                for(int i = 0; i < dim; i++){
+                    for(int j = 0; j < dim; j++){
+                        j > i ? cout << 0 << " " : cout << matrix[i * (i + 1) / 2 + j] << " ";
+                    }
+                    cout << endl;
+                }
+            }
+            else{
+                for(int i = 0; i < dim; i++){
+                    for(int j = 0; j < dim; j++){
+                        i > j ? cout << 0 << " " : cout << matrix[i * (i + 1) / 2 + j] << " ";
+                    }
+                    cout << endl;
+                }
+            }
+        }
+
+    private:
+
+        int *matrix;
+        int dim;
+        int size;
+        bool isLowerTriangular;
+};
+
+int main(){
+    int n;
+    cin >> n;
+    int **A = new int *[n];
+    for(int i = 0; i < n; i++)
+        A[i] = new int[n];
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> A[i][j];
+
+    TriangularMatrix tm(A, n);
+    tm.print();
+}
