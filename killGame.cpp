e982b0a0a9793e7b5f8d35e70a627edbb3f87d3a
atena <ashenatena@gmail.com>
2016-10-11 14:56:21 +0330

    HW#4-commented

---
+++ killGame.cpp
@@ -0,0 +1,43 @@
+//Ashena Gorgan Mohammadi, 610394128
+
+/* This is called kill game. The game is as this:
+ * n people are sitted round. Starting from the first player and moving forward
+ * for with the number of intervals, a player is killed each time. The person
+ * who remains alive is the winner. This game is implemented with a link array.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+string killGame(string name[], int n, int interval){
+    int next[n];
+    for(int i = 0; i < n - 1; i++)
+        next[i] = i + 1;
+    next[n - 1] = 0;
+    int remainers = n, i = n - 1, j = 0;
+    while(remainers != 1){
+        for(int k = 0; k < interval; k++){
+            i = j;
+            j = next[j];
+        }
+        next[i] = next[j];
+        remainers--;
+    }
+    return name[i];
+}
+
+int main(){
+    int n, interval;
+    cout << "Enter number of players:" << endl;
+    cin >> n;
+    string name[n];
+    cout << "Enter name of players:" << endl;
+    for(int i = 0; i < n; i++)
+        cin >> name[i];
+    cout << "Enter the kill interval:" << endl;
+    cin >> interval;
+    string winner = killGame(name, n, interval);
+    cout << "the winner is " << winner << endl;
+    return 0;
+}
