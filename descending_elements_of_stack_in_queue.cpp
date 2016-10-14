aa27fdd7604c7fb028517b8039331957df5e0839
atena <ashenatena@gmail.com>
2016-10-14 14:40:34 +0330

    HW#4-debuged

--- descending_elements_of_stack_in_queue.cpp
+++ descending_elements_of_stack_in_queue.cpp
@@ -14,35 +14,48 @@ using namespace std;
 
 template<class T> queue<T> sort(stack<T> S){
     queue<T> ans;
-    while(!S.empty()){
-    	int min = S.top();
-        S.pop();
-        while(!S.empty() && S.top() >= min){
+    //while(!S.empty()){
+    	//int min = S.top();
+        //S.pop();
+        while(!S.empty()){
             int aid = S.top();
             S.pop();
-        	while(ans.front() > aid){
+            if(ans.empty()){
+                ans.push(aid);
+                continue;
+            }
+            int c = 0;
+        	while(c < ans.size() && ans.front() > aid){
+                c++;
                 ans.push(ans.front());
                 ans.pop();
             }
             ans.push(aid);
-        	while(aid >= ans.front()){
+            c = 0;
+            while(c < ans.size() && ans.front() == aid){
+                c++;
+                ans.push(ans.front());
+        		ans.pop();
+            }
+        	while(c < ans.size() && aid > ans.front()){
+                c++;
         		ans.push(ans.front());
         		ans.pop();
         	}
         }
-        ans.push(min);
-    }
+        //ans.push(min);
+    //}
     return ans;
 }
 
 int main(){
     stack<int> S;
-    S.push(-1);
-    S.push(2);
-    S.push(-3);
-    S.push(-1);
     S.push(1);
+    S.push(2);
+    S.push(3);
+    S.push(4);
     S.push(5);
+    
     queue<int> ans = sort(S);
     while(!ans.empty()){
         cout << ans.front() << " ";
