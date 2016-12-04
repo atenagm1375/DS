db6d9636bf1c13b640512fd2def8eeb258a01f94
atena <ashenatena@gmail.com>
2016-12-04 17:03:40 +0330

    PROJECT#1-user friendly-not commented

---
+++ allShortestPaths.java
@@ -0,0 +1,96 @@
+// Ashena Gorgan Mohammadi, 610394128
+
+/* This is one of the projects for data structure course autumn-2016.
+ * The task is to find the shortest path between each two vertices in a directed
+ * graph and print that path.
+ */
+
+import java.util.Scanner;
+
+public class allShortestPaths{
+
+    public static Scanner scan = new Scanner(System.in);
+    public static int v;
+    public static int[] d;
+    public static boolean[] included;
+    public static int[] parent;
+
+    public static int[][] createGraph(){
+        int[][] graph = new int[v][v];
+        System.out.printf("From\t To\t Distance\t Enter -1 -1 -1 to end input\n");
+        int i, j;
+        do{
+            i = scan.nextInt();
+            j = scan.nextInt();
+            int a = scan.nextInt();
+            if(a != -1)
+                graph[i - 1][j - 1] = a;
+            else
+                break;
+        }while(i != -1 && j != -1);
+        return graph;
+    }
+
+    public static int minIndex(){
+        int min = Integer.MAX_VALUE, i = -1;
+        for(int u = 0; u < v; u++){
+            if(!included[u] && d[u] <= min && d[u] >= 0){
+                min = d[u];
+                i = u;
+            }
+        }
+        if(min == Integer.MAX_VALUE)
+            return -1;
+        return i;
+    }
+
+    public static void printPath(int i){
+        if(parent[i] == -1)
+            return;
+        printPath(parent[i]);
+        System.out.printf(" -> %d", i + 1);
+    }
+
+    public static void print(int u){
+        for(int i = 0; i < v; i++)
+            if(i != u && d[i] != Integer.MAX_VALUE){
+                System.out.printf("%2d %8d %14d %16d", u + 1, i + 1, d[i], u + 1);
+                printPath(i);
+                System.out.println();
+            }
+    }
+
+    public static void dijkstra(int[][] graph, int u){
+        d = new int[v];
+        included = new boolean[v];
+        parent = new int[v];
+        for(int i = 0; i < v; i++){
+            d[i] = Integer.MAX_VALUE;
+            included[i] = false;
+            parent[i] = -1;
+        }
+        d[u] = 0;
+        for(int i = 0; i < v; i++){
+            int w = minIndex();
+            if(w != -1){
+                included[w] = true;
+                for(int j = 0; j < v; j++)
+                    if(!included[j] && graph[w][j] != 0 && d[w] + graph[w][j] < d[j]){
+                        parent[j] = w;
+                        d[j] = d[w] + graph[w][j];
+                    }
+            }
+        }
+        print(u);
+    }
+    
+    public static void main(String[] args){
+        System.out.println("Enter the number of vertices:");
+        v = scan.nextInt();
+        int[][] graph = new int[v][v];
+        graph = createGraph();
+        System.out.printf("From\t To\t Minimum Distance\t Path\n");
+        for(int i = 0; i < v; i++)
+            dijkstra(graph, i);
+    }
+}
